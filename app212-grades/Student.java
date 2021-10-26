ffce592923ab294665fc770d48e43237139d83bb
ak123786 <ak5210365@gmail.com>
2021-10-26 15:09:52 +0100

    test 02

--- app212-grades/Student.java
+++ app212-grades/Student.java
@@ -104,7 +104,11 @@ public class Student
     
     private void printModules()
     {
-
+        for(ModuleMark mark: marks)
+        {
+            mark.print();
+            System.out.println("\t" + course.convertToGrade(mark.getValue()));
+        }
     }
     
     public void printTranscript()
@@ -122,6 +126,7 @@ public class Student
         System.out.println(" Code \t Module \t\tCredit\t Mark \t Grade");
         System.out.println(" ---- \t -------------------- \t ------\t ---- \t -----");
         
+        printModules();
        
         Grades finalGrade = course.calculateGrade(marks);
         
