8152ea3836c354aa2d3f3cc375acb9f23fced4c8
ak123786 <ak5210365@gmail.com>
2021-11-23 16:53:10 +0000

    app214

--- app214-stock/StockApp.java
+++ app214-stock/StockApp.java
@@ -4,8 +4,8 @@
  * stock manager so that users can add, edit,
  * print and remove stock products
  *
- * @author Student Name
- * @version 0.1
+ * @author Akes Ali
+ * @version 23/11/2021
  */
 public class StockApp
 {
@@ -49,25 +49,107 @@ public class StockApp
         {
             return true;
         }
+        else if(choice.equals("add"))
+        {
+            addProduct();
+        }
         else if(choice.equals("print"))
         {
-            //stock.print();
+            stock.print();
         }
         
+        else if(choice.equals("remove"))
+        {
+            removeProduct();
+        }
+        else if(choice.equals("buy"))
+        {
+            buyProduct();
+        }
+        else if(choice.equals("sell"))
+        {
+            sellProduct();
+        } 
+        else if(choice.equals("search"))
+        {
+            searchProduct();
+        }
+        else if(choice.equals("low stock"))
+        {
+            stock.printLowStock();
+        }
+        else if(choice.equals("restock"))
+        {
+            stock.lowStockList();
+        }
         return false;
     }
-   
+    
+    public void addProduct()
+    {
+        int id = reader.getInt("Please enter the ID: ");
+        String name = reader.getString("Please enter the name of the product: ");
+        Product product = new Product(id, name);
+        stock.add(product);
+        System.out.println("Product " + product.getID() 
+        + ", " + product.getName() + " has been ADDED");
+    }    
+    
+    public void removeProduct()
+    {
+        int id = reader.getInt("Please enter the ID: ");
+        stock.removeProductViaID(id);
+        System.out.println("Product has been removed");
+    }
+    
+    public void buyProduct()
+    {
+        int id = reader.getInt("Please enter the ID: ");
+        int amount = reader.getInt("Please enter sell amount >");  
+  
+        Product product = stock.findProduct(id);
+        stock.sellProduct(id, amount);
+        stock.print();    
+        System.out.println("Sold " + amount + " of " 
+        + product.getName());
+    }
+    
+    public void sellProduct()
+    {
+        int id = reader.getInt("Please enter the ID: ");
+        int amount = reader.getInt("Please enter sell amount >");
+            
+        Product product = stock.findProduct(id);
+        stock.sellProduct(id, amount);
+        stock.print();
+            
+        System.out.println("Sold " + amount + " of " 
+        + product.getName());
+    } 
+    
+    public void searchProduct()
+    {
+        int id = reader.getInt("Please enter the ID: ");
+        Product product = stock.findProduct(id);
+        product.getName();
+        System.out.println("Found " + product.getName());
+    }
+    
     /**
      * Print out a menu of operation choices
      */
     private void printMenuChoices()
     {
         System.out.println();
+        System.out.println("    Print:      Print all product");
+        System.out.println("    Quit:       Quit the program");
         System.out.println("    Add:        Add a new product");
         System.out.println("    Remove:     Remove an old product");
-        System.out.println("    Print:      Print all products");
-        System.out.println("    Quit:       Quit the program");
-        System.out.println();        
+        System.out.println("    Buy:        Buy a product");
+        System.out.println("    Sell:       Sell a product");
+        System.out.println("    Search:     Search for a product");
+        System.out.println("    Low Stock:  List of low stock");
+        System.out.println("    Restock:    Restock all low stock");
     }
     
     /**
@@ -77,7 +159,7 @@ public class StockApp
     {
         System.out.println("********************************");
         System.out.println("  App21-04: Stock Application ");
-        System.out.println("      by Student Name");
+        System.out.println("      by AK");
         System.out.println("********************************");
     }
 }
